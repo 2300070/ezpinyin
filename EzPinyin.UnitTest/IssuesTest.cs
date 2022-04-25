8e19ee278a6478e392f185ce913b8bd88dfd865a
sean <dexiang.wu@gmail.com>
2022-04-25 23:55:43 +0800

    完成相关问题的单元测试，改进文档中的描述信息。

--- EzPinyin.UnitTest/IssuesTest.cs
+++ EzPinyin.UnitTest/IssuesTest.cs
@@ -7,6 +7,12 @@ namespace EzPinyin.UnitTest
 	public class IssuesTest
 	{
 		[TestMethod]
+		public void VerifyIssue3()
+		{
+			//此处由于在test_issues3_dict.txt文件中虽然定义了新拼音，但由于不具备访问该文件的权限，因此此处应该返回原来的拼音且测试过程不能报错。
+			Console.WriteLine(PinyinHelper.GetPinyin("北市").AssertToBe("bei shi"));
+		}
+		[TestMethod]
 		public void VerifyIssue2()
 		{
 			DateTime start = DateTime.Now;
