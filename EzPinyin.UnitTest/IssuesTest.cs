98ee0913fbd219214ed0a2e4a65fba9a49b29af2
2300070 <2300070@gmail.com>
2023-04-13 11:16:30 +0800

    修正一些描述性文字

--- EzPinyin.UnitTest/IssuesTest.cs
+++ EzPinyin.UnitTest/IssuesTest.cs
@@ -9,6 +9,7 @@ namespace EzPinyin.UnitTest
 		[TestMethod]
 		public void VerifyIssue3()
 		{
+			//用于确认之前因无法访问本地文件而抛出的异常得以解决。
 			//测试前需要去除test_issues3_dict.txt的访问权限。
 			//此处由于在test_issues3_dict.txt文件中虽然定义了新拼音，但由于不具备访问该文件的权限，因此此处应该返回原来的拼音且测试过程不能报错。
 			Console.WriteLine(PinyinHelper.GetPinyin("北市").AssertToBe("bei shi"));
@@ -16,11 +17,13 @@ namespace EzPinyin.UnitTest
 		[TestMethod]
 		public void VerifyIssue2()
 		{
+			//用于确认之前初始加载速度过慢的问题得以改进。
 			DateTime start = DateTime.Now;
 			Console.WriteLine(PinyinHelper.GetPinyin("川藏"));
 			Console.WriteLine($"{DateTime.Now - start}");
 			Console.WriteLine(PinyinHelper.GetPinyin("Hello,World!!"));
 			Console.WriteLine(PinyinHelper.GetPinyin("世界你好!!Hello,World!!"));
+			Console.WriteLine(PinyinHelper.GetPinyin("世界你好Hello,World!!"));
 
 			Console.WriteLine(TinyPinyin.PinyinHelper.GetPinyin("世界你好!!Hello,World!!"));
 		}
